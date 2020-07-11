a1db1e7f4994cc68780298576730c8ca3ab6718d
Bruno <blais.bruno@gmail.com>
2020-07-11 16:48:08 -0400

    Fixed a bug with the postprocessing and the outputing of the table Renamed 3D test and slightly shrunk it down

--- source/solvers/navier_stokes_base.cc
+++ source/solvers/navier_stokes_base.cc
@@ -900,6 +900,7 @@ NavierStokesBase<dim, VectorType, DofsType>::postprocess(bool firstIter)
                 {
                   total_time += summary[it->first];
                 }
+
               this->error_table.add_value("total_time", total_time);
             }
           else
